e4c6a964f6007850c503b4f674a52437911b364b
Viwu <sinconsistencia@gmail.com>
2025-11-26 12:50:34 -0600

    cursor_block

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import suvindo.Block;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
@@ -8,16 +9,24 @@ class PlayState extends FlxState
 {
 	public var blocks:FlxTypedGroup<Block>;
 
+	public var cursor_block:Block;
+
 	override public function create()
 	{
 		super.create();
 
 		blocks = new FlxTypedGroup<Block>();
 		add(blocks);
+
+		cursor_block = new Block('plank');
+		add(cursor_block);
+		cursor_block.alpha = .5;
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		cursor_block.setPosition((FlxG.mouse.x / 16), (FlxG.mouse.y / 16));
 	}
 }
