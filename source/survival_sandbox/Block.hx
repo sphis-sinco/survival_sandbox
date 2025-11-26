aa0ed05840630b742f7f30c878dca51bc3209512
Viwu <sinconsistencia@gmail.com>
2025-11-26 12:45:03 -0600

    block class

---
+++ source/survival_sandbox/Block.hx
@@ -0,0 +1,15 @@
+package survival_sandbox;
+
+import flixel.FlxSprite;
+
+class Block extends FlxSprite
+{
+	public var block_id:String;
+
+	override public function new(block_id:String, ?X:Float, ?Y:Float)
+	{
+		super(X, Y);
+
+		loadGraphic('assets/images/blocks/' + block_id + '.png');
+	}
+}
