ff8f7b930a7cf549ffb4146b82ee273dd45259f1
Viwu <sinconsistencia@gmail.com>
2025-11-27 12:10:29 -0600

    music rate define

--- source/suvindo/Init.hx
+++ source/suvindo/Init.hx
@@ -21,6 +21,18 @@ class Init extends FlxState
 		}
 		FlxG.plugins.addPlugin(new ReloadPlugin());
 
+		#if (MUSIC_RATE == OFF)
+		TrackManager.MUSIC_RATE = OFF;
+		#elseif (MUSIC_RATE == CONSTANT)
+		TrackManager.MUSIC_RATE = CONSTANT;
+		#elseif (MUSIC_RATE == DEFAULT)
+		TrackManager.MUSIC_RATE = DEFAULT;
+		#elseif (MUSIC_RATE == FREQUENT)
+		TrackManager.MUSIC_RATE = FREQUENT;
+		#elseif (MUSIC_RATE == VARIABLE)
+		TrackManager.MUSIC_RATE = VARIABLE;
+		#end
+
 		#if RESOURCE_PACK_MENU
 		FlxG.switchState(() -> new ResourcePackMenu());
 		#else
