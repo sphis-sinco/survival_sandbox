061847b21516083eaa6f08ff0f91fa7564300ba5
Viwu <sinconsistencia@gmail.com>
2025-11-27 07:54:09 -0600

    fix pos of world info

--- source/suvindo/DebugWorldSelection.hx
+++ source/suvindo/DebugWorldSelection.hx
@@ -87,7 +87,7 @@ class DebugWorldSelection extends FlxState
 			FlxG.resetState();
 		});
 
-		worldInfo = new FlxText(FlxG.width / 2, 2, FlxG.width / 2, '', 32);
+		worldInfo = new FlxText(FlxG.width / 2, 2 + world_name.height + world_name.y, FlxG.width / 2, '', 32);
 		add(worldInfo);
 		worldInfo.alignment = RIGHT;
 		worldInfo.scrollFactor.set();
