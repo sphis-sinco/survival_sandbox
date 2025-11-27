6d4fddb6bc1c16e44bca26c46e49545d4c03d016
Viwu <sinconsistencia@gmail.com>
2025-11-26 21:37:45 -0600

    variations type is loaded

--- source/suvindo/Block.hx
+++ source/suvindo/Block.hx
@@ -1,5 +1,6 @@
 package suvindo;
 
+import flixel.system.FlxAssets.FlxGraphicAsset;
 import haxe.Json;
 #if sys
 import sys.FileSystem;
@@ -17,6 +18,7 @@ class Block extends FlxSprite
 	public var block_json:BlockJSON;
 
 	public var variation_index:Int = 0;
+	public var variation_graphics:Array<FlxGraphicAsset> = [];
 
 	public var hsv_shader:HSVShader;
 
@@ -30,14 +32,29 @@ class Block extends FlxSprite
 		this.shader = hsv_shader;
 	}
 
-	public function switchBlock(new_block:String)
+	public function defaultLoadGraphic(new_block:String)
 	{
 		#if sys
 		loadGraphic(FlxGraphic.fromBitmapData(BitmapData.fromFile(ResourcePacks.getPath('images/blocks/' + new_block + '.png'))));
 		#else
 		loadGraphic(ResourcePacks.getPath('images/blocks/' + new_block + '.png'));
 		#end
+	}
+
+	public function changeVariationIndex(amount:Int)
+	{
+		variation_index += amount;
+
+		if (variation_index < 0)
+			variation_index = 0;
+		if (variation_index > variation_graphics.length - 1)
+			variation_index = variation_graphics.length - 1;
+	}
+
+	public function switchBlock(new_block:String)
+	{
 		variation_index = 0;
+		variation_graphics = [];
 
 		block_json = null;
 		if (#if !sys Assets.exists #else FileSystem.exists #end (ResourcePacks.getPath('images/blocks/' + new_block + '.json')))
@@ -47,11 +64,33 @@ class Block extends FlxSprite
 			#else
 			block_json = cast Json.parse(Assets.getText(ResourcePacks.getPath('images/blocks/' + new_block + '.json')));
 			#end
-			if (block_json != null)
+			if (block_json != null && block_json.type != null)
 			{
-				if (block_json.type != null) {}
+				switch (block_json.type.toLowerCase())
+				{
+					case 'variations':
+						for (variation in block_json.variations)
+						{
+							#if sys
+							var variation_graphic:FlxGraphicAsset = FlxGraphic.fromBitmapData(BitmapData.fromFile(ResourcePacks.getPath('images/'
+								+ variation.texture + '.png')));
+							#else
+							var variation_graphic:FlxGraphicAsset = ResourcePacks.getPath('images/' + variation.texture + '.png');
+							#end
+
+							variation_graphics.push(variation_graphic);
+						}
+
+						loadGraphic(variation_graphics[variation_index]);
+					default:
+						defaultLoadGraphic(new_block);
+				}
 			}
+			else
+				defaultLoadGraphic(new_block);
 		}
+		else
+			defaultLoadGraphic(new_block);
 
 		if (this.graphic != null)
 			this.scale.set(1 * (16 / this.graphic.width), 1 * (16 / this.graphic.height));
