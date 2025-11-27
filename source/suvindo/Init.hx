36b01651d3b26eb4463017cdd819e1b61420bd4c
Viwu <sinconsistencia@gmail.com>
2025-11-27 07:07:58 -0600

    Not sys only

--- source/suvindo/Init.hx
+++ source/suvindo/Init.hx
@@ -9,22 +9,18 @@ class Init extends FlxState
 	{
 		super.create();
 
-        FlxG.mouse.visible = false;
-        ReloadPlugin.onReloadInit = () -> {
-            ReloadPlugin.reload.add(ResourcePacks.reload);
-            ReloadPlugin.reload.add(BlockList.reload);
-        }
+		FlxG.mouse.visible = false;
+		ReloadPlugin.onReloadInit = () ->
+		{
+			ReloadPlugin.reload.add(ResourcePacks.reload);
+			ReloadPlugin.reload.add(BlockList.reload);
+		}
 		FlxG.plugins.addPlugin(new ReloadPlugin());
-		
 
 		#if RESOURCE_PACK_MENU
 		FlxG.switchState(() -> new ResourcePackMenu());
 		#else
-		#if (sys)
 		FlxG.switchState(() -> new DebugWorldSelection());
-		#else
-		FlxG.switchState(() -> new PlayState());
-		#end
 		#end
 	}
 }
